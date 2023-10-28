8e5d451b2777b6882781c10784f0b14aace959fc
Ueudje <classlol72@gmail.com>
2023-10-28 10:16:01 -0700

    done

--- rsa.py
+++ rsa.py
@@ -1,31 +1,33 @@
-#!/usr/bin/python3
-
-import sys
+#!/usr/bin/env /usr/bin/python3
 
 
-def factor(line):
-    number = int(line)
-    value = 0
-    if number < 4:
-        print("{:d}={:d}*1".format(number, number))
-        return
-    for i in range(2, number):
-        if number % i == 0:
-            value = number // i
-            break
-    print("{:d}={:d}*{:d}".format(number, value, i))
 
 
-if len(sys.argv) != 2:
-    print("Usage: factors <file>")
-    sys.exit()
-filename = sys.argv[1]
-try:
-    test = open(filename, "r")
-except FileNotFoundError:
-    print("Error: Can't open file <{:s}>".format(filename))
-    sys.exit()
-line = test.read()
-factor(line)
+def factorize_all(line):
+    if line <= 1:
+        return None, None
+    if line % 2 == 0:
+        if is_prime(line // 2):
+            print("{}={}*{}".format(line, line // 2, 2))
+            return
+    else:
+        for i in range(3, line, 2):
+            if line % i == 0:
+                if is_prime(line // i):
+                    print("{}={}*{}".format(line, line // i, i))
+                    return
+    return None, None
 
-test.close()
+def is_prime(num):
+    if num <= 1:
+        return False
+    if num <= 3:
+        return True
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
